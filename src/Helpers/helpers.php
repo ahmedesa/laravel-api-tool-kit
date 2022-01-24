f6333244161876c1c38a0ce63c0542912879bc21
Sinan Dalal <sinanabom@gmail.com>
2022-01-24 10:08:42 +0300

    Add a condition to check if the class name is of a model type

--- src/Helpers/helpers.php
+++ src/Helpers/helpers.php
@@ -1,22 +1,24 @@
 <?php
-if (! function_exists('createOrRandomFactory')) {
+if (!function_exists('createOrRandomFactory')) {
     /**
      * Get a random item from the given model $class_name or create a new one by factory
      *
-     * @param string $class_name
-     * @return object|stdClass instance of a $class_name or stdClass
+     * @param string $class_name must be type model
+     * @return object instance of a $class_name or stdClass
+     * @throws Exception
      */
     function createOrRandomFactory(string $class_name): object
     {
-        if (is_subclass_of($class_name, 'Illuminate\Database\Eloquent\Model')) {
-            $class = new $class_name();
-            if ($class::count()) {
-                return $class::inRandomOrder()->first();
-            }
-            if (class_exists($class_name . 'Factory')) {
-                return $class_name::factory()->create();
-            }
+        if (!is_subclass_of($class_name, 'Illuminate\Database\Eloquent\Model')) {
+            throw new \Exception('parameter class_name is not a model type');
         }
-        return new stdClass();
+
+        $class = new $class_name();
+
+        if ($class::count()) {
+            return $class::inRandomOrder()->first();
+        }
+
+        return $class_name::factory()->create();
     }
 }

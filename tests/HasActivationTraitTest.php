7e1c4b0aaba1b959a3f2e22a4dcd5e9f87e122b6
ahmedesa <ahmedesa6120@gmail.com>
2023-08-08 17:41:38 +0300

    add test for HasActivation Trait

---
+++ tests/HasActivationTraitTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Essa\APIToolKit\Tests;
+
+use Essa\APIToolKit\Tests\Mocks\Models\TestModel;
+
+class HasActivationTraitTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function activeScopeReturnsOnlyActiveRecords()
+    {
+        TestModel::factory(3)->create([
+            'is_active' => true,
+        ]);
+
+        TestModel::factory(7)->create([
+            'is_active' => false,
+        ]);
+
+        $activeRecords = TestModel::active()->get();
+
+        $this->assertCount(3, $activeRecords);
+    }
+
+    /**
+     * @test
+     */
+    public function toggleActivationChangesStatus()
+    {
+        $model = TestModel::factory()->create([
+            'is_active' => true,
+        ]);
+
+        $model->toggleActivation();
+
+        $this->assertFalse($model->is_active);
+
+        $model->toggleActivation();
+
+        $this->assertTrue($model->is_active);
+    }
+}

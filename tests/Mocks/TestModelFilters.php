88fb740bd34390f754eaaf18ff5eaf383f401822
ahmed <ahmedesa6120@gmail.com>
2023-08-09 08:13:07 +0300

    add Date And Time Filters test

--- tests/Mocks/TestModelFilters.php
+++ tests/Mocks/TestModelFilters.php
@@ -3,9 +3,14 @@
 namespace Essa\APIToolKit\Tests\Mocks;
 
 use Essa\APIToolKit\Filters\QueryFilters;
+use Essa\APIToolKit\Traits\DateFilter;
+use Essa\APIToolKit\Traits\TimeFilter;
 
 class TestModelFilters extends QueryFilters
 {
+    use DateFilter;
+    use TimeFilter;
+
     protected array $columnSearch = ['name'];
     protected array $allowedFilters = ['id'];
 }

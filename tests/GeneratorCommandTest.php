f4d2eb8baaed92f2a92785098a2e4eb384cea30a
ahmedesa <ahmedesa6120@gmail.com>
2023-08-10 12:15:38 +0300

    add more test cases to GeneratorCommandTest

--- tests/GeneratorCommandTest.php
+++ tests/GeneratorCommandTest.php
@@ -7,6 +7,18 @@ class GeneratorCommandTest extends TestCase
     /**
      * @test
      */
+    public function reservedNameValidation()
+    {
+        $this->artisan('api:generate', [
+            'model' => 'class',
+        ])
+            ->expectsOutput('The name "class" is reserved by PHP.')
+            ->assertExitCode(0);
+    }
+
+    /**
+     * @test
+     */
     public function generateCommandWithAllDefaults()
     {
         $this->artisan('api:generate', [
@@ -32,12 +44,65 @@ class GeneratorCommandTest extends TestCase
     /**
      * @test
      */
-    public function reservedNameValidation()
+    public function generateCommandWithoutDefaultOptionsButWithSoftDelete()
     {
         $this->artisan('api:generate', [
-            'model' => 'class',
+            'model' => 'CustomSoftDeleteModel',
         ])
-            ->expectsOutput('The name "class" is reserved by PHP.')
+            ->expectsQuestion('Select all default options ?', 'y')
+            ->expectsChoice('Do you want to use <options=bold>soft delete</> ?', 'y', ['Yes', 'No', 'n', 'y'])
             ->assertExitCode(0);
+
+        $this->assertStringContainsString('SoftDeletes', file_get_contents(app_path('Models/CustomSoftDeleteModel.php')));
+        $this->assertStringContainsString('permanent-delete', file_get_contents(base_path('routes/api.php')));
+        $this->assertStringContainsString('restore', file_get_contents(base_path('routes/api.php')));
+        $this->assertStringContainsString('forceDelete', file_get_contents(app_path('Http/Controllers/API/CustomSoftDeleteModelController.php')));
+    }
+
+    public function generateCommandWithoutDefaultOptionsButWithoutSoftDelete()
+    {
+        $this->artisan('api:generate', [
+            'model' => 'CustomModel',
+        ])
+            ->expectsQuestion('Select all default options ?', 'y')
+            ->expectsChoice('Do you want to use <options=bold>soft delete</> ?', 'n', ['Yes', 'No', 'n', 'y'])
+            ->assertExitCode(0);
+
+        $this->assertStringNotContainsString('SoftDeletes', file_get_contents(app_path('Models/CustomModel.php')));
+        $this->assertStringNotContainsString('permanent-delete', file_get_contents(base_path('routes/api.php')));
+        $this->assertStringNotContainsString('restore', file_get_contents(base_path('routes/api.php')));
+        $this->assertStringNotContainsString('forceDelete', file_get_contents(app_path('Http/Controllers/API/CustomModelController.php')));
+    }
+
+    /**
+     * @test
+     */
+    public function generateCommandWithoutDefaultOptions()
+    {
+        $this->artisan('api:generate', [
+            'model' => 'WithoutDefaultNewCustomModel',
+        ])
+            ->expectsQuestion('Select all default options ?', 'n')
+            ->expectsChoice('Do you want to use <options=bold>soft delete</> ?', 'n', ['Yes', 'No', 'n', 'y'])
+            ->expectsChoice('Do you want to generate <options=bold>controller</> ?', 'n', ['Yes', 'No', 'n', 'y'])
+            ->expectsChoice('Do you want to generate <options=bold>request</> ?', 'n', ['Yes', 'No', 'n', 'y'])
+            ->expectsChoice('Do you want to generate <options=bold>resource</> ?', 'y', ['Yes', 'No', 'n', 'y'])
+            ->expectsChoice('Do you want to generate <options=bold>migration</> ?', 'y', ['Yes', 'No', 'n', 'y'])
+            ->expectsChoice('Do you want to generate <options=bold>factory</> ?', 'n', ['Yes', 'No', 'n', 'y'])
+            ->expectsChoice('Do you want to generate <options=bold>seeder</> ?', 'y', ['Yes', 'No', 'n', 'y'])
+            ->expectsChoice('Do you want to generate <options=bold>filter</> ?', 'n', ['Yes', 'No', 'n', 'y'])
+            ->expectsChoice('Do you want to generate <options=bold>test</> ?', 'y', ['Yes', 'No', 'n', 'y'])
+            ->expectsChoice('Do you want to generate <options=bold>routes</> ?', 'y', ['Yes', 'No', 'n', 'y'])
+            ->assertExitCode(0);
+
+        $this->assertFileExists(app_path('Models/WithoutDefaultNewCustomModel.php'));
+        $this->assertFileExists(app_path('Http/Resources/WithoutDefaultNewCustomModel/WithoutDefaultNewCustomModelResource.php'));
+        $this->assertFileExists(database_path('migrations'));
+        $this->assertFileExists(database_path('seeders/WithoutDefaultNewCustomModelSeeder.php'));
+        $this->assertFileExists(base_path('tests/Feature/WithoutDefaultNewCustomModelTest.php'));
+
+        $this->assertFileDoesNotExist(app_path('Http/Controllers/API/WithoutDefaultNewCustomModelController.php'));
+        $this->assertFileDoesNotExist(database_path('factories/WithoutDefaultNewCustomModelFactory.php'));
+        $this->assertFileDoesNotExist(app_path('Filters/WithoutDefaultNewCustomModelFilters.php'));
     }
 }

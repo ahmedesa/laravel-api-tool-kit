8e9b7e4e8ffee9cb9530f6d86400a11b33fd0d40
Ahmed Esa <ahmedesa6120@gmail.com>
2023-08-10 12:19:51 +0300

    Unit tests enhancements (#26)
    * add more test cases to ApiResponseTest
    
    * add more test cases to DynamicPaginateTest
    
    * add more test cases to MediaHelperTest
    
    * add more test cases to GeneratorCommandTest

--- tests/ApiResponseTest.php
+++ tests/ApiResponseTest.php
@@ -47,6 +47,93 @@ class ApiResponseTest extends TestCase
         $this->assertEquals(['data' => 'value'], (array) $responseData->data);
     }
 
+    public function responseUnprocessable()
+    {
+        $response = $this->classThatImplementTheTrait->responseUnprocessable('Validation failed', 'Unprocessable entity');
+
+        $this->assertInstanceOf(JsonResponse::class, $response);
+        $this->assertEquals(Response::HTTP_UNPROCESSABLE_ENTITY, $response->getStatusCode());
+        $responseData = $response->getData();
+        $this->assertEquals('Validation failed', $responseData->errors[0]->detail);
+    }
+
+    /**
+     * @test
+     */
+    public function responseBadRequest()
+    {
+        $response = $this->classThatImplementTheTrait->responseBadRequest('Invalid request', 'Bad request');
+
+        $this->assertInstanceOf(JsonResponse::class, $response);
+        $this->assertEquals(Response::HTTP_BAD_REQUEST, $response->getStatusCode());
+        $responseData = $response->getData();
+        $this->assertEquals('Invalid request', $responseData->errors[0]->detail);
+    }
+
+    /**
+     * @test
+     */
+    public function responseDeleted()
+    {
+        $response = $this->classThatImplementTheTrait->responseDeleted();
+
+        $this->assertInstanceOf(JsonResponse::class, $response);
+        $this->assertEquals(Response::HTTP_NO_CONTENT, $response->getStatusCode());
+        $this->assertEquals('{}', $response->getContent());
+    }
+
+    /**
+     * @test
+     */
+    public function responseNotFound()
+    {
+        $response = $this->classThatImplementTheTrait->responseNotFound('Resource not found', 'Not found');
+
+        $this->assertInstanceOf(JsonResponse::class, $response);
+        $this->assertEquals(Response::HTTP_NOT_FOUND, $response->getStatusCode());
+        $responseData = $response->getData();
+        $this->assertEquals('Resource not found', $responseData->errors[0]->detail);
+    }
+
+    /**
+     * @test
+     */
+    public function responseUnAuthorized()
+    {
+        $response = $this->classThatImplementTheTrait->responseUnAuthorized('Unauthorized', 'Unauthorized access');
+
+        $this->assertInstanceOf(JsonResponse::class, $response);
+        $this->assertEquals(Response::HTTP_FORBIDDEN, $response->getStatusCode());
+        $responseData = $response->getData();
+        $this->assertEquals('Unauthorized', $responseData->errors[0]->detail);
+    }
+
+    /**
+     * @test
+     */
+    public function responseUnAuthenticated()
+    {
+        $response = $this->classThatImplementTheTrait->responseUnAuthenticated('Unauthenticated', 'Authentication required');
+
+        $this->assertInstanceOf(JsonResponse::class, $response);
+        $this->assertEquals(Response::HTTP_UNAUTHORIZED, $response->getStatusCode());
+        $responseData = $response->getData();
+        $this->assertEquals('Unauthenticated', $responseData->errors[0]->detail);
+    }
+
+    /**
+     * @test
+     */
+    public function responseConflictError()
+    {
+        $response = $this->classThatImplementTheTrait->responseConflictError('Conflict', 'Resource conflict');
+
+        $this->assertInstanceOf(JsonResponse::class, $response);
+        $this->assertEquals(Response::HTTP_CONFLICT, $response->getStatusCode());
+        $responseData = $response->getData();
+        $this->assertEquals('Conflict', $responseData->errors[0]->detail);
+    }
+
     /**
      * @test
      */

27478848f4f2fa2a5d26dd12035f5c896afe5c44
ahmedesa <ahmedesa6120@gmail.com>
2023-08-10 16:53:10 +0300

    refactor GeneratorCommand

--- src/Commands/GeneratorCommand.php
+++ src/Commands/GeneratorCommand.php
@@ -2,27 +2,28 @@
 
 namespace Essa\APIToolKit\Commands;
 
+use Essa\APIToolKit\Generator\Handlers\ComponentCreatorHandler;
+use Essa\APIToolKit\Generator\Handlers\UserChoicesHandler;
 use Essa\APIToolKit\Generator\StubParser;
 use Illuminate\Console\Command;
-use Illuminate\Filesystem\Filesystem;
-use Illuminate\Support\Facades\Artisan;
-use Illuminate\Support\Str;
 
 class GeneratorCommand extends Command
 {
-    protected array $allOptions = [
-        'controller',
-        'request',
-        'resource',
-        'migration',
-        'factory',
-        'seeder',
-        'filter',
-        'test',
-        'routes',
-    ];
+    protected $signature = 'api:generate {model}
+                            {--m|migration}
+                            {--c|controller}
+                            {--R|request}
+                            {--r|resource}
+                            {--s|seeder}
+                            {--f|factory}
+                            {--F|filter}
+                            {--t|test}
+                            {--routes}
+                            {--soft-delete}';
 
-    protected array $reservedNames = [
+    protected $description = 'This command generate api crud.';
+
+    private array $reservedNames = [
         '__halt_compiler',
         'abstract',
         'and',
@@ -93,29 +94,16 @@ class GeneratorCommand extends Command
         'yield',
     ];
 
-    protected $signature = 'api:generate {model}
-                            {--m|migration}
-                            {--c|controller}
-                            {--R|request}
-                            {--r|resource}
-                            {--s|seeder}
-                            {--f|factory}
-                            {--F|filter}
-                            {--t|test}
-                            {--routes}
-                            {--soft-delete}';
+    private UserChoicesHandler $userChoicesHandler;
 
-    protected $description = 'This command generate api crud.';
+    private ComponentCreatorHandler $componentCreatorHandler;
 
-    private string $model;
-    private Filesystem $filesystem;
-    private StubParser $stubParser;
-
-    public function __construct(Filesystem $filesystem)
+    public function __construct(UserChoicesHandler $userChoicesHandler, ComponentCreatorHandler $componentCreatorHandler)
     {
-        $this->filesystem = $filesystem;
+        $this->userChoicesHandler = $userChoicesHandler;
 
         parent::__construct();
+        $this->componentCreatorHandler = $componentCreatorHandler;
     }
 
     public function handle()
@@ -128,198 +116,21 @@ class GeneratorCommand extends Command
             return false;
         }
 
-        $this->model = $model;
+        $userChoices = $this->userChoicesHandler
+            ->setCommand($this)
+            ->handel();
 
-        $this->getUserChoices();
-
-        $this->stubParser = new StubParser($model, $this->options());
-
-        $this->createModel();
-
-        $this->createComponents();
-
-        $this->updateRoutes();
+        $this->componentCreatorHandler
+            ->setModel($model)
+            ->setUserChoices($userChoices)
+            ->setStubParser(new StubParser($model, $userChoices))
+            ->handle();
 
         $this->info('Module created successfully!');
     }
 
-    private function createComponents(): void
-    {
-        if ($this->option('controller')) {
-            $this->createController();
-        }
-
-        if ($this->option('filter')) {
-            $this->createFilter();
-        }
-
-        if ($this->option('resource')) {
-            $this->createResources();
-        }
-
-        if ($this->option('test')) {
-            $this->createTest();
-        }
-
-        if ($this->option('migration')) {
-            $this->createMigration();
-        }
-
-        if ($this->option('factory')) {
-            $this->createFactory();
-        }
-
-        if ($this->option('request')) {
-            $this->createRequest();
-        }
-
-        if ($this->option('seeder')) {
-            $this->createSeeder();
-        }
-    }
-
-    private function createController(): void
-    {
-        if (! file_exists(app_path('/Http/Controllers/API'))) {
-            $this->filesystem->makeDirectory(app_path('/Http/Controllers/API'));
-        }
-
-        file_put_contents(app_path("Http/Controllers/API/{$this->model}Controller.php"), $this->stubParser->parseStub('DummyController'));
-    }
-
-    private function createModel(): void
-    {
-        file_put_contents(app_path("Models/{$this->model}.php"), $this->stubParser->parseStub('Dummy'));
-    }
-
-    private function createTest(): void
-    {
-        if (! file_exists(base_path('tests/Feature/'))) {
-            $this->filesystem->makeDirectory(base_path('tests/Feature/'));
-        }
-
-        file_put_contents(base_path("tests/Feature/{$this->model}Test.php"), $this->stubParser->parseStub('DummyTest'));
-    }
-
-    private function createFilter(): void
-    {
-        if (! file_exists(app_path('/Filters'))) {
-            $this->filesystem->makeDirectory(app_path('/Filters'));
-        }
-
-        file_put_contents(app_path("Filters/{$this->model}Filters.php"), $this->stubParser->parseStub('DummyFilters'));
-    }
-
-    private function createResources(): void
-    {
-        if (! file_exists(app_path('/Http/Resources'))) {
-            $this->filesystem->makeDirectory(app_path('/Http/Resources'));
-        }
-
-        if (! file_exists(app_path('/Http/Resources/' . $this->model))) {
-            $this->filesystem->makeDirectory(app_path('/Http/Resources/' . $this->model));
-        }
-
-        file_put_contents(
-            app_path("Http/Resources/{$this->model}/{$this->model}Resource.php"),
-            $this->stubParser->parseStub('DummyResource')
-        );
-    }
-
-    private function createMigration(): void
-    {
-        Artisan::call('make:migration', [
-            'name' => 'create_' . Str::plural(Str::snake($this->model)) . '_table',
-        ]);
-    }
-
-    private function createFactory(): void
-    {
-        Artisan::call('make:factory', [
-            'name' => $this->model . 'Factory',
-            '--model' => $this->model,
-        ]);
-    }
-
-    private function createRequest(): void
-    {
-        Artisan::call('make:request', [
-            'name' => "{$this->model}\Create{$this->model}Request",
-        ]);
-
-        Artisan::call('make:request', [
-            'name' => "{$this->model}\Update{$this->model}Request",
-        ]);
-    }
-
-    private function createSeeder(): void
-    {
-        Artisan::call('make:seeder', [
-            'name' => $this->model . 'Seeder',
-        ]);
-    }
-
-    private function updateRoutes(): void
-    {
-        if ($this->option('routes')) {
-            $this->filesystem->append(
-                base_path('routes/api.php'),
-                $this->stubParser->parseStub('routes')
-            );
-        }
-    }
-
     private function isReservedName($name): bool
     {
-        $name = strtolower($name);
-
-        return in_array($name, $this->reservedNames);
-    }
-
-    private function getUserChoices(): void
-    {
-        $yesOrNo = [
-            'y' => 'Yes',
-            'n' => 'No',
-        ];
-
-        $allDefaultSelected = $this->choice(
-            'Select all default options ?',
-            $yesOrNo,
-            'y'
-        );
-
-        $choice = $this->choice(
-            'Do you want to use <options=bold>soft delete</> ?',
-            $yesOrNo,
-            'y'
-        );
-
-        $this->input->setOption('soft-delete', $choice == 'y');
-
-        if ($allDefaultSelected == 'y') {
-            $this->setDefaultOptions();
-
-            return;
-        }
-
-        foreach ($this->allOptions as $option) {
-            $choice = $this->choice(
-                "Do you want to generate <options=bold>{$option}</> ?",
-                $yesOrNo,
-                'y'
-            );
-
-            $this->input->setOption($option, $choice == 'y');
-        }
-    }
-
-    private function setDefaultOptions(): void
-    {
-        foreach (config('api-tool-kit.default_generates') as $option) {
-            if (in_array($option, $this->allOptions)) {
-                $this->input->setOption($option, true);
-            }
-        }
+        return in_array(strtolower($name), $this->reservedNames);
     }
 }

f839e8aed3dec10682979fadd9055dfcabcb5ff8
ahmed <ahmedesa6120@gmail.com>
2023-08-22 15:07:01 +0300

    use stubs to generate all components instead of artisan in some parts

--- src/Generator/Handlers/ComponentCreatorHandler.php
+++ src/Generator/Handlers/ComponentCreatorHandler.php
@@ -4,7 +4,6 @@ namespace Essa\APIToolKit\Generator\Handlers;
 
 use Essa\APIToolKit\Generator\StubParser;
 use Illuminate\Filesystem\Filesystem;
-use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Str;
 
 class ComponentCreatorHandler
@@ -18,8 +17,6 @@ class ComponentCreatorHandler
 
     public function handle(): void
     {
-        $this->generateArtisanCommands();
-
         $this->generateTheComponents();
     }
 
@@ -35,18 +32,6 @@ class ComponentCreatorHandler
         $this->userChoices = $userChoices;
 
         return $this;
-    }
-
-    private function generateArtisanCommands(): void
-    {
-        $commandsToGenerate = $this->commandsToGenerate();
-
-        foreach ($commandsToGenerate as $commands) {
-
-            if ($this->userChoices[$commands->type]) {
-                Artisan::call($commands->name, $commands->options);
-            }
-        }
     }
 
     private function generateTheComponents(): void
@@ -83,50 +68,35 @@ class ComponentCreatorHandler
         file_put_contents($component->path, $stubParser->parseStub($component->stub));
     }
 
-    private function commandsToGenerate(): array
-    {
-        return [
-            new CommandInfo(
-                type: 'migration',
-                name: 'make:migration',
-                options: [
-                    'name' => 'create_' . Str::plural(Str::snake($this->model)) . '_table',
-                ]
-            ),
-            new CommandInfo(
-                type: 'factory',
-                name: 'make:factory',
-                options: [
-                    'name' => "{$this->model}Factory",
-                    '--model' => $this->model,
-                ]
-            ),
-            new CommandInfo(
-                type: 'seeder',
-                name: 'make:seeder',
-                options: [
-                    'name' => "{$this->model}Seeder",
-                ]
-            ),
-            new CommandInfo(
-                type: 'request',
-                name: 'make:request',
-                options: [
-                    'name' => "{$this->model}\Create{$this->model}Request",
-                ]
-            ),
-            new CommandInfo(
-                type: 'request',
-                name: 'make:request',
-                options: [
-                    'name' => "{$this->model}\Update{$this->model}Request",
-                ]
-            ),
-        ];
-    }
     private function componentsToGenerate(): array
     {
+        $migrationFileName = $this->getMigrationTableName();
+
         return [
+            new ComponentInfo(
+                type: 'model',
+                folder: app_path('/Models'),
+                path: app_path("Models/{$this->model}.php"),
+                stub: 'Dummy'
+            ),
+            new ComponentInfo(
+                type: 'factory',
+                folder: database_path('/factories'),
+                path: database_path("factories/{$this->model}Factory.php"),
+                stub: 'DummyFactory'
+            ),
+            new ComponentInfo(
+                type: 'seeder',
+                folder: database_path('/seeders'),
+                path: database_path("seeders/{$this->model}Seeder.php"),
+                stub: 'DummySeeder'
+            ),
+            new ComponentInfo(
+                type: 'migration',
+                folder: database_path('/migrations'),
+                path: database_path("migrations/{$migrationFileName}"),
+                stub: 'dummy_migration'
+            ),
             new ComponentInfo(
                 type: 'controller',
                 folder: app_path('/Http/Controllers/API'),
@@ -152,11 +122,24 @@ class ComponentCreatorHandler
                 stub: 'DummyResource'
             ),
             new ComponentInfo(
-                type: 'model',
-                folder: app_path('/Models'),
-                path: app_path("Models/{$this->model}.php"),
-                stub: 'Dummy'
+                type: 'request',
+                folder: app_path('/Http/Requests/' . $this->model),
+                path: app_path("Http/Requests/{$this->model}/Create{$this->model}Request.php"),
+                stub: 'CreateDummyRequest'
+            ),
+            new ComponentInfo(
+                type: 'request',
+                folder: app_path('/Http/Requests/' . $this->model),
+                path: app_path("Http/Requests/{$this->model}/Update{$this->model}Request.php"),
+                stub: 'UpdateDummyRequest'
             ),
         ];
+    }
+
+    private function getMigrationTableName(): string
+    {
+        $migrationClass = 'create_' . Str::plural(Str::snake($this->model)) . '_table';
+
+        return date('Y_m_d_His') . "_{$migrationClass}.php";
     }
 }

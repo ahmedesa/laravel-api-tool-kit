2eb7053438357785977b0c021903259d111c243c
ahmed <ahmedesa6120@gmail.com>
2023-08-23 09:50:07 +0300

    delete ComponentCreatorHandler.php

--- src/Commands/GeneratorCommand.php
+++ src/Commands/GeneratorCommand.php
@@ -2,9 +2,12 @@
 
 namespace Essa\APIToolKit\Commands;
 
-use Essa\APIToolKit\Generator\ComponentCreatorHandler;
+use Essa\APIToolKit\Generator\DTOs\ComponentInfo;
 use Essa\APIToolKit\Generator\SchemaParser;
+use Essa\APIToolKit\Generator\StubParser;
 use Illuminate\Console\Command;
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\Support\Str;
 
 class GeneratorCommand extends Command
 {
@@ -94,20 +97,22 @@ class GeneratorCommand extends Command
         'yield',
     ];
 
-    private ComponentCreatorHandler $componentCreatorHandler;
+    private const DEFAULT_VALUES = [
+        'model',
+    ];
 
     public function __construct(
-        ComponentCreatorHandler $componentCreatorHandler
+        private Filesystem $filesystem,
     )
     {
         parent::__construct();
-
-        $this->componentCreatorHandler = $componentCreatorHandler;
     }
 
     public function handle()
     {
         $model = ucfirst($this->argument('model'));
+
+        $this->model = ucfirst($this->argument('model'));
 
         if ($this->isReservedName($this->argument('model'))) {
             $this->error('The name "' . $this->argument('model') . '" is reserved by PHP.');
@@ -121,11 +126,23 @@ class GeneratorCommand extends Command
 
         $schemaParserOutput = $schemaParser->parse();
 
-        $this->componentCreatorHandler
-            ->setModel($model)
-            ->setUserChoices($userChoices)
-            ->setSchemaParserOutput($schemaParserOutput)
-            ->handle();
+        $stubParser = new StubParser(
+            model: $model,
+            options: $userChoices,
+            schemaParserOutput: $schemaParserOutput
+        );
+
+        $componentsToGenerate = $this->componentsToGenerate();
+
+        foreach ($componentsToGenerate as $component) {
+            if (in_array($component->type, self::DEFAULT_VALUES) || $userChoices[$component->type]) {
+                $this->createComponent($component, $stubParser);
+            }
+        }
+
+        if ($this->option('routes')) {
+            $this->appendRoutes($stubParser);
+        }
 
         $this->info('Module created successfully!');
     }
@@ -151,5 +168,97 @@ class GeneratorCommand extends Command
     private function isReservedName($name): bool
     {
         return in_array(mb_strtolower($name), $this->reservedNames);
+    }
+
+    private function appendRoutes(StubParser $stubParser): void
+    {
+        $this->filesystem->append(
+            base_path('routes/api.php'),
+            $stubParser->parseStub('routes')
+        );
+    }
+
+    private function createComponent(ComponentInfo $component, StubParser $stubParser): void
+    {
+        if (!file_exists($component->folder)) {
+            $this->filesystem->makeDirectory($component->folder, 0777, true, true);
+        }
+
+        file_put_contents($component->path, $stubParser->parseStub($component->stub));
+    }
+
+    private function componentsToGenerate(): array
+    {
+        $migrationFileName = $this->getMigrationTableName();
+
+        return [
+            new ComponentInfo(
+                type: 'model',
+                folder: app_path('/Models'),
+                path: app_path("Models/{$this->model}.php"),
+                stub: 'Dummy'
+            ),
+            new ComponentInfo(
+                type: 'factory',
+                folder: database_path('/factories'),
+                path: database_path("factories/{$this->model}Factory.php"),
+                stub: 'DummyFactory'
+            ),
+            new ComponentInfo(
+                type: 'seeder',
+                folder: database_path('/seeders'),
+                path: database_path("seeders/{$this->model}Seeder.php"),
+                stub: 'DummySeeder'
+            ),
+            new ComponentInfo(
+                type: 'migration',
+                folder: database_path('/migrations'),
+                path: database_path("migrations/{$migrationFileName}"),
+                stub: 'dummy_migration'
+            ),
+            new ComponentInfo(
+                type: 'controller',
+                folder: app_path('/Http/Controllers/API'),
+                path: app_path("Http/Controllers/API/{$this->model}Controller.php"),
+                stub: 'DummyController'
+            ),
+            new ComponentInfo(
+                type: 'test',
+                folder: base_path('tests/Feature/'),
+                path: base_path("tests/Feature/{$this->model}Test.php"),
+                stub: 'DummyTest'
+            ),
+            new ComponentInfo(
+                type: 'filter',
+                folder: app_path('/Filters'),
+                path: app_path("Filters/{$this->model}Filters.php"),
+                stub: 'DummyFilters'
+            ),
+            new ComponentInfo(
+                type: 'resource',
+                folder: app_path('/Http/Resources/' . $this->model),
+                path: app_path("Http/Resources/{$this->model}/{$this->model}Resource.php"),
+                stub: 'DummyResource'
+            ),
+            new ComponentInfo(
+                type: 'request',
+                folder: app_path('/Http/Requests/' . $this->model),
+                path: app_path("Http/Requests/{$this->model}/Create{$this->model}Request.php"),
+                stub: 'CreateDummyRequest'
+            ),
+            new ComponentInfo(
+                type: 'request',
+                folder: app_path('/Http/Requests/' . $this->model),
+                path: app_path("Http/Requests/{$this->model}/Update{$this->model}Request.php"),
+                stub: 'UpdateDummyRequest'
+            ),
+        ];
+    }
+
+    private function getMigrationTableName(): string
+    {
+        $migrationClass = 'create_' . Str::plural(Str::snake($this->model)) . '_table';
+
+        return date('Y_m_d_His') . "_{$migrationClass}.php";
     }
 }

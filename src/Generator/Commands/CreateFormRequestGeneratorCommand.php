4f147e38774b15c67ff05c54c69ee3e0500aa4fb
ahmedesa <ahmedesa6120@gmail.com>
2023-08-25 09:50:30 +0300

    improve api-tool-kit config file

--- src/Generator/Commands/CreateFormRequestGeneratorCommand.php
+++ src/Generator/Commands/CreateFormRequestGeneratorCommand.php
@@ -15,14 +15,10 @@ class CreateFormRequestGeneratorCommand extends GeneratorCommand implements Sche
             'createValidationRules' => (new CreateValidationRulesParser($this->apiGenerationCommandInputs->getSchema()))->parse(),
         ];
     }
+
     protected function getStubName(): string
     {
         return 'CreateDummyRequest';
-    }
-
-    protected function getOutputFolderPath(): string
-    {
-        return app_path("Http/Requests/{$this->apiGenerationCommandInputs->getModel()}");
     }
 
     protected function getOutputFilePath(): PathResolverInterface
